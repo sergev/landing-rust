e82492e9ab98b9e9f5b00c222dfeffb604a7a756
Serge Vakulenko <serge.vakulenko@gmail.com>
2020-08-17 00:23:08 -0700

    Compute gravity and thrust.

--- optimize.cpp
+++ optimize.cpp
@@ -2,13 +2,25 @@
 // Compute an optimal landing control sequence.
 //
 #include <iostream>
+#include <iomanip>
 #include "landing.h"
 #include "interact.h"
 #include "catch/catch.hpp"
 
 static std::string reply;
-static int initial_weight; // lbs
-static int initial_fuel;   // lbs
+static int initial_weight_lbs;
+static int initial_fuel_lbs;
+
+static int time_sec;
+static int altitude_feet;
+static int velocity_fps;
+static int fuel_lbs;
+static int prev_time_sec;
+static int prev_altitude_feet;
+static int prev_velocity_fps;
+static int prev_fuel_lbs;
+
+static const int FEET_PER_MILE = 5280;
 
 //
 // Return the first n characters of a string.
@@ -46,10 +58,8 @@ std::string last_n(std::string &str, unsigned n)
 // Make sure the beginning of the reply matches `first' string.
 // Make sure the tail of the reply matches `last' string.
 //
-void check_reply(Interact &session, const std::string &first, const std::string &last)
+void check_reply(const std::string &first, const std::string &last)
 {
-    reply = session.receive();
-
     if (first.size() > 0 &&
         first_n(reply, first.size()) != first)
         throw std::runtime_error("Wrong output, expect: " + first);
@@ -57,24 +67,6 @@ void check_reply(Interact &session, const std::string &first, const std::string 
     if (last.size() > 0 &&
         last_n(reply, last.size()) != last)
         throw std::runtime_error("Wrong prompt, expect: " + last);
-}
-
-//
-// Read reply.
-// Rememer it as last_reply for later processing.
-// Make sure the beginning of the reply matches `first' string.
-// Make sure the tail of the reply matches `last' string.
-// Send the `send' command.
-//
-void check_and_send(Interact &session, const std::string &first, const std::string &last, const std::string &send)
-{
-    check_reply(session, first, last);
-
-    // Debug output.
-    // Comment out this for less verbosity.
-    std::cout << reply << send;
-
-    session.send(send);
 }
 
 //
@@ -92,7 +84,21 @@ void send_and_check(Interact &session, const std::string &send, const std::strin
 
     session.send(send);
 
-    check_reply(session, first, last);
+    reply = session.receive();
+    check_reply(first, last);
+}
+
+//
+// Somewhere in the string is located a number with a preceeding text.
+// Find and return the number.
+//
+int fetch_value(std::string &str, const std::string &text)
+{
+    auto pos = str.find(text);
+    assert(pos != std::string::npos);
+
+    auto value = str.substr(pos + text.size());
+    return std::stoi(value);
 }
 
 //
@@ -100,130 +106,46 @@ void send_and_check(Interact &session, const std::string &send, const std::strin
 //
 void capture_weight_and_fuel()
 {
-    //TODO
+    initial_weight_lbs = fetch_value(reply, "WEIGHT IS NOW");
+    initial_fuel_lbs = fetch_value(reply, "YOU HAVE");
 }
 
-void try1()
+//
+// Extract values of time, altitude, velocity and fuel.
+//
+//    0          120       0     5280        16000
+//
+void capture_state(std::string str)
 {
-    Interact session(game);
+    // Save previous state.
+    prev_time_sec = time_sec;
+    prev_altitude_feet = altitude_feet;
+    prev_velocity_fps = velocity_fps;
+    prev_fuel_lbs = fuel_lbs;
 
-    check_and_send(session, "", "YOURS ? ", "0\n");
-    check_and_send(session, "", "RATE= ", "t1200\n");
-    check_and_send(session, "", "RATE= ", "200\n");
+    // Scan the line.
+    std::stringstream input (str);
+    int miles, feet;
+    input >> time_sec >> miles >> feet >> velocity_fps >> fuel_lbs;
+    assert(!!input);
 
-    std::string prompt = "RATE= ";
-    for (;;) {
-        reply = session.receive();
-
-        if (last_n(reply, prompt.size()) != prompt) {
-            std::cout << reply;
-            break;
-        }
-
-        std::string send = "73\n";
-        std::cout << reply << send;
-
-        session.send(send);
-    }
-    // FUEL OUT AT 202 SECONDS.
-    // ON THE MOON AT 202 SECONDS.
-    // IMPACT VELOCITY OF 482 F.P.S.
-    // FUEL LEFT 0 LBS.
-    // THERE'S NOW A NEW LUNAR CRATER 91 FEET DEEP.
+    // Convert miles into feet.
+    altitude_feet = miles * FEET_PER_MILE + feet;
 }
 
-void try2()
+//
+// From the last reply, extract values of time, altitude, velocity and fuel.
+//
+// TIME(SEC) ALT(MILES + FEET) VELOCITY(FPS) FUEL(LBS)
+//    0          120       0     5280        16000
+//
+void capture_state0()
 {
-    Interact session(game);
+    std::string text = "VELOCITY(FPS) FUEL(LBS)";
+    auto pos = reply.find(text);
+    assert(pos != std::string::npos);
 
-    check_and_send(session, "", "YOURS ? ", "0\n");
-    check_and_send(session, "", "RATE= ", "t1200\n");
-    check_and_send(session, "", "RATE= ", "200\n");
-    check_and_send(session, "", "RATE= ", "200\n");
-
-    std::string prompt = "RATE= ";
-    for (;;) {
-        reply = session.receive();
-
-        if (last_n(reply, prompt.size()) != prompt) {
-            std::cout << reply;
-            break;
-        }
-
-        std::string send = "72\n";
-        std::cout << reply << send;
-
-        session.send(send);
-    }
-    // FUEL OUT AT 203 SECONDS.
-    // ON THE MOON AT 205 SECONDS.
-    // IMPACT VELOCITY OF 497 F.P.S.
-    // FUEL LEFT 0 LBS.
-    // THERE'S NOW A NEW LUNAR CRATER 94 FEET DEEP.
-}
-
-void try3()
-{
-    Interact session(game);
-
-    check_and_send(session, "", "YOURS ? ", "1\n");
-    check_and_send(session, "", "RATE= ", "t1200\n");
-    check_and_send(session, "", "RATE= ", "200\n");
-    check_and_send(session, "", "RATE= ", "200\n");
-    check_and_send(session, "", "RATE= ", "200\n");
-    check_and_send(session, "", "RATE= ", "200\n");
-    check_and_send(session, "", "RATE= ", "200\n");
-    check_and_send(session, "", "RATE= ", "200\n");
-    check_and_send(session, "", "RATE= ", "200\n");
-    check_and_send(session, "", "RATE= ", "200\n");
-    check_and_send(session, "", "RATE= ", "200\n");
-    check_and_send(session, "", "RATE= ", "200\n");
-    check_and_send(session, "", "RATE= ", "200\n");
-    check_and_send(session, "", "RATE= ", "200\n");
-    check_and_send(session, "", "RATE= ", "200\n");
-    check_and_send(session, "", "RATE= ", "200\n");
-    check_and_send(session, "", "RATE= ", "200\n");
-    check_and_send(session, "", "RATE= ", "200\n");
-    check_and_send(session, "", "RATE= ", "200\n");
-    check_and_send(session, "", "RATE= ", "200\n");
-    check_and_send(session, "", "RATE= ", "200\n");
-    check_and_send(session, "", "RATE= ", "200\n");
-    check_and_send(session, "", "RATE= ", "200\n");
-    check_and_send(session, "", "RATE= ", "200\n");
-    check_and_send(session, "", "RATE= ", "200\n");
-    check_and_send(session, "", "RATE= ", "200\n");
-    check_and_send(session, "", "RATE= ", "200\n");
-    check_and_send(session, "", "RATE= ", "200\n");
-    check_and_send(session, "", "RATE= ", "200\n");
-    check_and_send(session, "", "RATE= ", "200\n");
-    check_and_send(session, "", "RATE= ", "200\n");
-    check_and_send(session, "", "RATE= ", "200\n");
-    check_and_send(session, "", "RATE= ", "200\n");
-    check_and_send(session, "", "RATE= ", "200\n");
-    check_and_send(session, "", "RATE= ", "200\n");
-    check_and_send(session, "", "RATE= ", "200\n");
-    check_and_send(session, "", "RATE= ", "200\n");
-    check_and_send(session, "", "RATE= ", "200\n");
-    check_and_send(session, "", "RATE= ", "200\n");
-    check_and_send(session, "", "RATE= ", "200\n");
-    check_and_send(session, "", "RATE= ", "200\n");
-    check_and_send(session, "", "RATE= ", "200\n");
-    check_and_send(session, "", "RATE= ", "200\n");
-
-    std::string prompt = "RATE= ";
-    for (;;) {
-        reply = session.receive();
-
-        if (last_n(reply, prompt.size()) != prompt) {
-            std::cout << reply;
-            break;
-        }
-
-        std::string send = "27\n";
-        std::cout << reply << send;
-
-        session.send(send);
-    }
+    capture_state(reply.substr(pos + text.size()));
 }
 
 //
@@ -232,10 +154,76 @@ void try3()
 TEST_CASE("level2", "[landing]")
 {
     Interact session(game);
-    check_reply(session, "MISSION CONTROL", "YOURS ? ");
 
-    send_and_check(session, "2\n", "", "RATE= ");
+    //-------------------------
+    // Select level 2.
+    //
+    reply = session.receive();
+    check_reply("MISSION CONTROL", "YOURS ? ");
+    send_and_check(session, "2\n", "YOU MUST SPECIFY", "RATE= ");
+
+    // Get initial weight and fuel supply.
     capture_weight_and_fuel();
-    REQUIRE(initial_weight != 0);
-    REQUIRE(initial_fuel != 0);
+    REQUIRE(initial_weight_lbs != 0);
+    REQUIRE(initial_fuel_lbs != 0);
+    std::cout << "Initial weight, lbs: " << initial_weight_lbs << "\n";
+    std::cout << "Initial fuel, lbs: " << initial_fuel_lbs << "\n";
+
+    // Get initial altitude and velocity.
+    capture_state0();
+    REQUIRE(time_sec == 0);
+    REQUIRE(altitude_feet > 0);
+    REQUIRE(velocity_fps > 0);
+    REQUIRE(fuel_lbs == initial_fuel_lbs);
+    std::cout << "Altitude, feet: " << altitude_feet << "\n";
+    std::cout << "Velocity, fps: " << velocity_fps << "\n";
+
+    //-------------------------
+    // Free fall for 10 second.
+    //
+    std::string prompt = "RATE= ";
+    send_and_check(session, "0\n", " ", prompt);
+
+    // Measure gravity.
+    capture_state(reply);
+    REQUIRE(time_sec == 10);
+    REQUIRE(altitude_feet < prev_altitude_feet);
+    REQUIRE(velocity_fps > prev_velocity_fps);
+    REQUIRE(fuel_lbs == prev_fuel_lbs);
+
+    double gravity_fpss = (velocity_fps - prev_velocity_fps) / 10.0;
+    std::cout << std::fixed << std::setprecision(1) << "Gravity, fpss: " << gravity_fpss << "\n";
+
+    //-------------------------
+    // Full thrust for 10 second.
+    //
+    send_and_check(session, "200\n", " ", prompt);
+
+    // Measure thrust.
+    capture_state(reply);
+    REQUIRE(time_sec == 10 + prev_time_sec);
+    REQUIRE(altitude_feet < prev_altitude_feet);
+    REQUIRE(velocity_fps < prev_velocity_fps);
+    REQUIRE(fuel_lbs < prev_fuel_lbs);
+
+    int weight_lbs = initial_weight_lbs - initial_fuel_lbs + prev_fuel_lbs; // or current fuel_lbs?
+    double thrust_fps = ((prev_velocity_fps - velocity_fps) / 10.0 + gravity_fpss) * weight_lbs / 200;
+    std::cout << std::fixed << std::setprecision(0) << "Thrust, fps: " << thrust_fps << "\n";
+
+    //
+    // Compute optimal thrust.
+    //
+    for (;;) {
+        int control = 72;
+
+std::cout << reply << control << "\n";
+        session.send(std::to_string(control) + "\n");
+        reply = session.receive();
+
+        if (last_n(reply, prompt.size()) != prompt)
+            break;
+
+        check_reply(" ", prompt);
+    }
+    std::cout << reply << std::endl;
 }
